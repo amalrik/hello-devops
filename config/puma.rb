6312d74e2c1341a904b4ab232454c72864608fdf
amalrik <amalrik.maia@gmail.com>
2017-09-18 18:32:15 -0300

    configs

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,6 @@
+pidfile "tmp/pids/puma.pid"
+state_path "tmp/pids/puma.state"
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -9,7 +12,11 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+if ENV['RAILS_ENV'] == 'production'
+  bind "unix://tmp/sockets/puma.sock"
+else
+  port 3000
+end
 
 # Specifies the `environment` that Puma will run in.
 #
